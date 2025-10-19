d804c68d08d62e18f1244bab533306dcbb3fc1b5
Tae Hoon Kim <taehoonkim@Tae-Hoons-MacBook-Pro.local>
2025-10-18 23:09:36 -0400

    fix sam's email

--- src/pages/ministries/Campus.jsx
+++ src/pages/ministries/Campus.jsx
@@ -6,7 +6,7 @@ export default function Campus() {
       title="Campus Ministry"
       description="The college years can be a time of great growth and challenge. It is in this time that so many of us find our identity and purpose. What will that be? Who will we be? What will we live for in this life? LGM Campus hopes to help you find your identity and purpose, and we hope you find it in Christ! We have a vibrant campus-based ministry for undergraduate students or college-aged or anyone who would like to be involved in ministry that takes place primarily on University of Michigan campus, regardless if you’re in school or not. You are welcome here at LGM, come check us out!"
       contactName="Sammy Kim"
-      contactEmail="samuelki@umich.edu"
+      contactEmail="samueki@umich.edu"
       imageSrc="/campus.JPG"
     />
   );
